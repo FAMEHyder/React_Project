a8b29043e9ce34b6d90e0abdc67a4fe806812518
FAMEHyder <famehyder9999@gmail.com>
2024-08-16 08:07:33 -0700

    react project

--- React-app/src/component/Footer.jsx
+++ React-app/src/component/Footer.jsx
@@ -7,8 +7,10 @@ const Footer = () => {
       <Container>
         <Typography variant="h6">Contact Us</Typography>
         <Typography>Email: Famehyder7777@gamil.com</Typography>
+        <Typography>Email: Famehyder9999@gamil.com</Typography>
         <Typography>Address: Sat_Tara Shoping Mall Skardu</Typography>
         <Typography>Phone: 03554526991</Typography>
+        <Typography>Phone: 03417405991</Typography>
       </Container>
     </Box>
   );
