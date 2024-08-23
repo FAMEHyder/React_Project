cbfc70793434c4034beaf2b3e234d7905ecd3f50
FAMEHyder <famehyder9999@gmail.com>
2024-08-23 05:34:57 -0700

    raect

--- My_project/src/Home.jsx
+++ My_project/src/Home.jsx
@@ -8,8 +8,8 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
 import IMG from "./image/Logo.png";
+import backgroundImage from './image/member.png';
 
 
 const Navbar = styled(AppBar)({
@@ -17,10 +17,11 @@ const Navbar = styled(AppBar)({
   left: 0,
   marginBottom: '20px',
   height: '60px',
-  marginTop:'5px',
-  marginLeft:'5px',
+  // marginTop:'5px',
+  marginLeft: '5px',
   zIndex: 1205,
-  background: 'linear-gradient(to right, rgb(51, 51, 196),rgb(6, 110, 180))', // blueGrey gradient
+  // background: 'linear-gradient(to right, rgb(51, 51, 196),rgb(6, 110, 180))', // blueGrey gradient
+  background: 'black',
 });
 
 const NavbarButton = styled(Button)(({ theme }) => ({
@@ -33,7 +34,7 @@ const SectionPaper = styled(Paper)({
   textAlign: 'center',
   background: 'skyblue',
   marginBottom: '4px',
-  marginTop:'30px',
+  marginTop: '30px',
   height: '200px',
   display: 'flex',
   alignItems: 'end',
@@ -48,7 +49,7 @@ const SectionPaper = styled(Paper)({
 const MainContainer = styled('div')({
   display: 'flex',
   marginTop: '64px',
-  
+
 });
 
 const Content = styled('main')({
@@ -62,29 +63,46 @@ function Home() {
 
   const handleSectionClick = (path) => {
     navigate(path);
-    
+
   };
 
   return (
     <>
       <Navbar>
         <Toolbar>
-            <img src={IMG} alt="pic" height={50} width={60} style={{marginBottom : '0px' }} />
-          <div style={{ marginBottom: '0px' , position : 'relative' ,left: '75%'}}>
+          <img src={IMG} alt="pic" height={50} width={60} style={{ marginBottom: '0px' }} />
+          <div style={{ marginBottom: '0px', position: 'relative', left: '75%' }}>
             <NavbarButton component={Link} to="/login">Login</NavbarButton>
             <NavbarButton component={Link} to="/register">Register</NavbarButton>
             <NavbarButton component={Link} to="/about">About</NavbarButton>
           </div>
         </Toolbar>
       </Navbar>
-      <MainContainer>
+      <MainContainer marginTop='100px'>
         <Content>
           <Container>
             <Grid container spacing={3}>
               <Grid item xs={12} sm={6} md={4}>
-                <SectionPaper onClick={() => handleSectionClick('/section/members')}>
+                <SectionPaper
+                  onClick={() => handleSectionClick('/section/members')}
+                  style={{
+                
+                    backgroundImage: `url(${backgroundImage})`,
+                    backgroundSize: 'cover ', // Ensure the image covers the entire area
+                    backgroundRepeat: 'no-repeat', // Prevent image repetition
+                    backgroundPosition: 'center', // Center the background image
+                    height: '69%', // Adjust the height as necessary
+                    width: '90%', // Adjust the width as necessary
+                    color: '#fff', // Optional: Set text color to ensure visibility on the background
+                    padding: '20px', // Optional: Add some padding to the content
+                    boxShadow : '3px 3px 3px 3px black'
+                    
+
+                  }}
+                >
                   <Typography variant="h5">Our Members</Typography>
                 </SectionPaper>
+
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
                 <SectionPaper onClick={() => handleSectionClick('/section/courses')}>
