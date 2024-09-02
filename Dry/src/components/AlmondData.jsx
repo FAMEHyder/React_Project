38138bb588e0df70a169580ce643224b2b7f2974
FAMEHyder <famehyder9999@gmail.com>
2024-09-02 07:36:35 -0700

    react

--- Dry/src/components/AlmondData.jsx
+++ Dry/src/components/AlmondData.jsx
@@ -1,6 +1,13 @@
 
 import { Grid, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import nonpareil from '../Image/nonpareil.png';
+import carmel from '../Image/carmel.png';
+import sonora from '../Image/sonora.png';
+import butte from '../Image/butte.png';
+import mission from '../Image/mission.png';
+import california from '../Image/california.png';
+import fritz from '../Image/fritz.png';
+import peerless from '../Image/peerless.png';
 const almondData = [
   {
     id: 1,
@@ -11,43 +18,43 @@ const almondData = [
   {
     id: 2,
     name: 'Carmel',
-    picture: 'url-to-picture-2',
+    picture: carmel,
     description: 'Similar to Nonpareil but with a slightly wrinkled surface, often used for roasting.',
   },
   {
     id: 3,
     name: 'Sonora',
-    picture: 'url-to-picture-3',
+    picture: sonora,
     description: 'Long, narrow almonds with a light color, commonly used in desserts.',
   },
   {
     id: 4,
     name: 'Butte',
-    picture: 'url-to-picture-4',
+    picture: butte,
     description: 'Thicker-skinned, good for roasting and used in confectionery.',
   },
   {
     id: 5,
     name: 'Mission',
-    picture: 'url-to-picture-5',
+    picture: mission,
     description: 'Darker skin, known for its slightly bittersweet flavor, often used in baking.',
   },
   {
     id: 6,
     name: 'California',
-    picture: 'url-to-picture-6',
+    picture: california,
     description: 'A category that includes several almond varieties, typically used for processed almond products.',
   },
   {
     id: 7,
     name: 'Fritz',
-    picture: 'url-to-picture-7',
+    picture: fritz,
     description: 'Small and hard, often used in almond paste and marzipan.',
   },
   {
     id: 8,
     name: 'Peerless',
-    picture: 'url-to-picture-8',
+    picture: peerless,
     description: 'Commonly used in snacks, known for its mild flavor.',
   },
 ];
@@ -60,7 +67,7 @@ const AlmondCards = () => {
           <Card>
             <CardMedia
               component="img"
-              height="140"
+              height="200"
               width={100}
               image={almond.picture}
               alt={almond.name}
