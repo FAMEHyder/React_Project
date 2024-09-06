cc41d6f022d1d460720a1eef1d86c5f237d7035e
FAMEHyder <famehyder9999@gmail.com>
2024-09-06 06:10:51 -0700

    react

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ app.use(express.json());
 
 
 app.get("/api", (req, res)=>{
-    res.json({message:"Hello from Backend"})
+    res.json({message:"Hello from Backend" ,starlight: "this is thor"})
+   
     })
     
 app.listen(8000, ()=>{
