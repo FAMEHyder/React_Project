b33dd588b39ad2e338e6474d3a2d0b7a22be53f8
FAMEHyder <famehyder9999@gmail.com>
2024-09-06 08:43:42 -0700

    react

--- Backend/server.js
+++ Backend/server.js
@@ -6,10 +6,22 @@ const app = express()
 app.use(express.json());
 
 
-app.get("/api", (req, res)=>{
-    res.json({message:"Hello from Backend" ,starlight: "this is thor"})
-   
+app.get("/apii", (req, res)=>{
+    res.json([{message:"Hello from asgard" ,starlight: "this is thor"},{message1:"Hello from earth" ,starlight: "this is ironman"}])
+    
     })
+
+app.post("/post/:reg",(req,res)=>{
+    const param = req.params.reg
+    const query = req.query.name
+    const body = req.body.gender
+    const data = `request from front end  ,param :${param},query :${query} ,gender ${gender}`
+
+    res.status(200).json ({message: "successfull",data : data})
+
+
+
+})
     
 app.listen(8000, ()=>{
     console.log("Server is Running")
