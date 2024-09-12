1d9dbd18912a85e636960fc70ae6932d0939ea9b
FAMEHyder <famehyder9999@gmail.com>
2024-09-12 05:59:24 -0700

    react

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import  express  from "express" 
 
-import { Register } from "../controllers/user.controller.js";
+import  Register  from '../Controllers/user.controller.js';
 const router = express.Router();
 
 
