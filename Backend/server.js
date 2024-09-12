d30f02e2d1f9b257b8b604d09204021786f98d93
FAMEHyder <famehyder9999@gmail.com>
2024-09-12 03:27:37 -0700

    react

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import router from './routes/user.routes.js';
 
 dotenv.config();
 
@@ -23,22 +24,24 @@ mongoose.connection.on('disconnected',()=>{
     console.log("disconnected")
 });
 
-app.get("/apii", (req, res)=>{
-    res.json([{message:"Hello from asgard" ,starlight: "this is thor"},{message1:"Hello from earth" ,starlight: "this is ironman"}])
+// app.get("/apii", (req, res)=>{
+//     res.json([{message:"Hello from asgard" ,starlight: "this is thor"},{message1:"Hello from earth" ,starlight: "this is ironman"}])
     
-    })
+//     })
 
-app.post("/post/:reg",(req,res)=>{
-    const param = req.params.reg
-    const query = req.query.name
+// app.post("/post/:reg",(req,res)=>{
+//     const param = req.params.reg
+//     const query = req.query.name
     
-    const addedData = `request from front end  ,param :${param},query :${query} `
+//     const addedData = `request from front end  ,param :${param},query :${query} `
 
-    res.status(200).json ({message: "successfull",data : addedData})
+//     res.status(200).json ({message: "successfull",data : addedData})
 
 
 
-})
+// })
+app.use('/user', router );
+
 const port = process.env.PORT || 8009
 app.listen(port, ()=>{
     DataBaseConnection();
