8598332bdee6dc2ae2e6ade253dcf2a8f69186b5
FAMEHyder <famehyder9999@gmail.com>
2024-09-12 10:30:06 -0700

    react

--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
+
 const register=async(req,res)=>{
 
     const {userName,email,firstName,lastName,password,...rest}=req.body
@@ -34,3 +35,26 @@ const register=async(req,res)=>{
 
 }
 export default register
+
+export const Login = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'Email Not Found' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid  password' });
+      }
+      const payload = { userId: user._id, role: user.roles[0] }; 
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' });
+    
+      res.cookie('access_token', token, { httpOnly: true }); 
+      res.status(200).json({ message: 'Login successful', Detail : user.email, });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Error logging in',  Error: err });
+    }
+  };
+  
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,12 +1,14 @@
 import  express  from "express" 
 
 import  Register  from '../Controllers/user.controller.js';
+import Login from '../Controllers/user.controller.js';
 const router = express.Router();
 
 
 
 
 router.post('/register',Register );
+router.post('/login',Login );
 
 
 
