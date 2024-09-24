48ecd42470b6a4bf129170f1650b6f29fa0ca9fa
FAMEHyder <famehyder9999@gmail.com>
2024-09-24 05:20:43 -0700

    react

--- Dry/src/components/PistachioData.jsx
+++ Dry/src/components/PistachioData.jsx
@@ -1,61 +1,61 @@
 import { Card, CardContent, CardMedia, Typography, Grid} from '@mui/material';
-import tilton from '../Image/w180.png'
-import moorpark from '../Image/jambo.png';
-import perfection from '../Image/w240.png';
-import blenheim from '../Image/w230.png';
-import goldkist from '../Image/w450.png';
-import chinese from '../Image/splites.png';
-import tomcot from '../Image/butts.png';
-import katy from '../Image/pieces.png';
+import kerman from '../Image/kerman.png'
+import antep from '../Image/antep.png';
+import fando from '../Image/fando.png';
+import bronte from '../Image/bronte.png';
+import kale from '../Image/kale.png';
+import badami from '../Image/badami.png';
+import Akbari from '../Image/akbari.png';
+import red from '../Image/redpista.png';
 
 
 const pistachioTypes = [
   { 
     id: 53,
     name: 'Kerman Pistachio',
-    image: moorpark,
+    image: kerman,
     description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making them a popular variety.',
   },
   { 
     id: 54,
     name: 'Turkish Antep Pistachio',
-    image: tilton,
+    image: antep,
     description: 'Turkish Antep pistachios are smaller with a strong flavor and are often used in Turkish desserts like baklava.',
   },
   { 
     id: 55,
     name: 'Iranian Fandoghi Pistachio',
-    image: perfection,
+    image: fando,
     description: 'Iranian Fandoghi pistachios are round and smaller, with a distinctive flavor that is popular in the Middle East.',
   },
   { 
     id: 56,
     name: 'Bronte Pistachio',
-    image: blenheim,
+    image: bronte,
     description: 'Bronte pistachios are grown in Sicily and are prized for their unique, vibrant green color and intense flavor.',
   },
   { 
     id: 57,
     name: 'Kalehghouchi Pistachio',
-    image: goldkist,
+    image: kale,
     description: 'Kalehghouchi pistachios are large and round, known for their open shells and rich taste.',
   },
   { 
     id: 58,
     name: 'Badami Pistachio',
-    image: chinese,
+    image: badami,
     description: 'Badami pistachios are long and thin, often used in Indian and Middle Eastern cuisines for their distinct flavor.',
   },
   { 
     id: 59,
     name: 'Akbari Pistachio',
-    image: tomcot,
+    image: Akbari,
     description: 'Akbari pistachios are long and large, with a smooth texture and mild, buttery flavor.',
   },
   { 
     id: 60,
     name: 'Red Pistachio',
-    image: katy,
+    image: red,
     description: 'Red pistachios are dyed with a bright red color and have a crunchy texture, making them popular as a snack.',
   },
 ];
