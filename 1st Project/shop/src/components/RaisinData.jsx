76e17fb319555a5ee164ac186e5d0b4b21fa5039
FAMEHyder <famehyder9999@gmail.com>
2024-11-10 16:44:51 +0500

    react

--- 1st Project/shop/src/components/RaisinData.jsx
+++ 1st Project/shop/src/components/RaisinData.jsx
@@ -102,7 +102,7 @@ const RaisinCards = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,  // Show one item at a time
+    slidesToShow: 1,  
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
