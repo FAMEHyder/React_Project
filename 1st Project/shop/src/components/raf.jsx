acb62cfb0dd6cd62c0e84f97466e2f4b204f76d7
FAMEHyder <famehyder9999@gmail.com>
2024-11-21 13:55:12 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -65,31 +65,6 @@ const ProductData = [
     rating: 4.5,   // Rating out of 5
 },
 
-//pischio data
-{
-  id: 53,
-  Category:'Raisins',
-  price:30,
-  name: 'Sultana Raisin',
-  image: sultana,
-  description: 'Sultana raisins are light yellow and known for their juicy and sweet flavor.',
-  
-    rating: 4.5,   // Rating out of 5
-
-},
-//walnutdata
-{
-  id: 65,
-  Category:'Walnuts',
-  price:23,
-  name: 'English Walnuts (Persian Walnuts)',
-  image: eng, // Replace with a valid image URL
-  description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
-  
-    rating: 4.5,   // Rating out of 5
-},
-];
-
 
 
 export default ProductData;
