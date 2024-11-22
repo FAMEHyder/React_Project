9890404c675f822dd9f88efde318f31007877e20
FAMEHyder <famehyder9999@gmail.com>
2024-11-22 13:59:54 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -23,13 +23,4 @@ const ProductData = [
     name: 'Nonpareil',
     image: nonpareil, // Image representing the Nonpareil almond variety
     description: 'Thin-skinned, easy to blanch, and used in many almond products. ' +
-                 'Overall, it is one of the most sought-after varieties in the market due to its versatility.',
-    price: 10.99,  // Price of the Nonpareil almond
-    rating: 4.5,   // Rating out of 5
-  },
-
-
-  // Appricote Data
-  { 
-    id: 9,
-    Categ
+        
