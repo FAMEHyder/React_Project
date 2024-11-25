8cb519f02a4c36c74419b888484bc375d49d17ed
FAMEHyder <famehyder9999@gmail.com>
2024-11-25 15:17:48 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -74,10 +74,32 @@ const ProductData = [
   image: kerman,
   description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making them a popular variety.',
   
+    rating: 4.5,   // Rating out of 5
+},
+
+//raisins data
+{
+  id: 53,
+  Category:'Raisins',
+  price:30,
+  name: 'Sultana Raisin',
+  image: sultana,
+  description: 'Sultana raisins are light yellow and known for their juicy and sweet flavor.',
+  
+    rating: 4.5,   // Rating out of 5
+
+},
 //walnutdata
 {
   id: 65,
   Category:'Walnuts',
+  price:23,
+  name: 'English Walnuts (Persian Walnuts)',
+  image: eng, // Replace with a valid image URL
+  description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
+  
+    rating: 4.5,   // Rating out of 5
+},
 ];
 
 
