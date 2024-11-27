ce53a9163c2bc3e4c6578989bb44e5a762d353b5
FAMEHyder <famehyder9999@gmail.com>
2024-11-27 15:19:25 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -13,6 +13,23 @@ import kerman from '../Image/kerman.png'
 import sultana from '../Image/sultana.png';
 
 
+
+import eng from "../Image/engwalnut.png";
+
+const ProductData = [
+  {
+    id: 1,
+    Category:'Almonds',
+    name: 'Nonpareil',
+    image: nonpareil, // Image representing the Nonpareil almond variety
+    description: 'Thin-skinned, easy to blanch, and used in many almond products. ' +
+                 'Overall, it is one of the most sought-after varieties in the market due to its versatility.',
+    price: 10.99,  // Price of the Nonpareil almond
+    rating: 4.5,   // Rating out of 5
+  },
+
+
+  // Appricote Data
   { 
     id: 9,
     Category:'Dried Apricots',
@@ -40,4 +57,53 @@ import sultana from '../Image/sultana.png';
 {
   id: 33,
   Category:'Dates',
+  price:50,
+  name: 'Medjool',
+  image: medjol, // Replace with actual image URLs
+  description: 'Large, soft, and rich in flavor, Medjool dates are known as the "king of dates".',
   
+    rating: 4.5,   // Rating out of 5
+},
+
+//pischio data
+{
+  id: 53,
+  Category:'Pistachios',
+  price:40,
+  name: 'Kerman Pistachio',
+  image: kerman,
+  description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making them a popular variety.',
+  
+    rating: 4.5,   // Rating out of 5
+},
+
+//raisins data
+{
+  id: 53,
+  Category:'Raisins',
+  price:30,
+  name: 'Sultana Raisin',
+  image: sultana,
+  description: 'Sultana raisins are light yellow and known for their juicy and sweet flavor.',
+  
+    rating: 4.5,   // Rating out of 5
+
+},
+//walnutdata
+{
+  id: 65,
+  Category:'Walnuts',
+  price:23,
+  name: 'English Walnuts (Persian Walnuts)',
+  image: eng, // Replace with a valid image URL
+  description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
+  
+    rating: 4.5,   // Rating out of 5
+},
+];
+
+
+
+export default ProductData;
+
+
