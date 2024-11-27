4dcd8ba1fce25842b75881f10f8583dd4c3f5e1a
FAMEHyder <famehyder9999@gmail.com>
2024-11-27 15:19:54 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -98,12 +98,3 @@ const ProductData = [
   image: eng, // Replace with a valid image URL
   description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
   
-    rating: 4.5,   // Rating out of 5
-},
-];
-
-
-
-export default ProductData;
-
-
