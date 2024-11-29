eb9f8ba387945d9d3bfbfcb6f1d242c9cff55125
FAMEHyder <famehyder9999@gmail.com>
2024-11-29 15:22:14 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -75,9 +75,3 @@ const ProductData = [
   description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making them a popular variety.',
   
     rating: 4.5,   // Rating out of 5
-},
-
-//raisins data
-{
-  id: 53,
-  Catego
