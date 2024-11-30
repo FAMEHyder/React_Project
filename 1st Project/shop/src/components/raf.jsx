d85fd89f45d5e66a83b53204c35a617b0a2b14cc
FAMEHyder <famehyder9999@gmail.com>
2024-11-30 15:23:13 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -76,14 +76,6 @@ const ProductData = [
   
     rating: 4.5,   // Rating out of 5
 },
-
-//raisins data
-{
-  id: 53,
-  Category:'Raisins',
-  price:30,
-  name: 'Sultana Raisin',
-  image: sultana,
   description: 'Sultana raisins are light yellow and known for their juicy and sweet flavor.',
   
     rating: 4.5,   // Rating out of 5
