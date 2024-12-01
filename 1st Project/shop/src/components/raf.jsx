e5a758e004c4a0807279c7ad32638169bcffe835
FAMEHyder <famehyder9999@gmail.com>
2024-12-01 15:24:48 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -9,19 +9,3 @@ import w180 from '../Image/w180.png'
 import medjol from '../Image/medjol.png';
 
 import kerman from '../Image/kerman.png'
-
-import sultana from '../Image/sultana.png';
-
-
-
-import eng from "../Image/engwalnut.png";
-
-const ProductData = [
-  {
-    id: 1,
-    Category:'Almonds',
-    name: 'Nonpareil',
-    image: nonpareil, // Image representing the Nonpareil almond variety
-    description: 'Thin-skinned, easy to blanch, and used in many almond products. ' +
-                 'Overall, it is one of the most sought-after varieties in the market due to its versatility.',
-    price: 10.99,  // Price of the Nonpareil almond
