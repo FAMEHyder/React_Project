8a7138a25336a2c15e6703b074ce549947696349
FAMEHyder <famehyder9999@gmail.com>
2024-12-06 15:31:12 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -66,22 +66,7 @@ const ProductData = [
 },
 
 //pischio data
-{
-  id: 53,
-  Category:'Pistachios',
-  description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making them a popular variety.',
-  
-    rating: 4.5,   // Rating out of 5
-},
-
-//raisins data
-{
-  id: 53,
-  Category:'Raisins',
-  price:30,
-  name: 'Sultana Raisin',
-  image: sultana,
-  description: 'Sultana raisins are light yellow and known for their juicy and sweet flavor.',
+{w and known for their juicy and sweet flavor.',
   
     rating: 4.5,   // Rating out of 5
 
