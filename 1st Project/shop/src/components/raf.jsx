c97ad93006d9edde869ed4a5a1d0daacd8a1bb79
FAMEHyder <famehyder9999@gmail.com>
2024-12-08 15:39:59 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -20,13 +20,6 @@ const ProductData = [
   {
     id: 1,
     Category:'Almonds',
-    name: 'Nonpareil',
-    image: nonpareil, // Image representing the Nonpareil almond variety
-    description: 'Thin-skin
-
-  // Appricote Data
-  { 
-    id: 9,
     Category:'Dried Apricots',
     price: 20,
     name: 'Moorpark Apricot',
