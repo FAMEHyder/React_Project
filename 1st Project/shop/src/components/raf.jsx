50b4c24e04023196cd07500baf5fc08e4716b2db
FAMEHyder <famehyder9999@gmail.com>
2024-12-11 15:35:12 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -8,28 +8,7 @@ import w180 from '../Image/w180.png'
 
 import medjol from '../Image/medjol.png';
 
-import kerman from '../Image/kerman.png'
-
-import sultana from '
-  id: 21,
-  Category:'Cashews',
-  price:120,
-  name: 'W-180 (King of Cashews)',
-  image: w180,
-  description: 'W-180, known as the "King of Cashews," is the largest and highest quality cashew nut available.',
-  
-    rating: 4.5,   // Rating out of 5
-},
-//Dates Data
-{
-  id: 33,
-  Category:'Dates',
-  price:50,
-  name: 'Medjool',
-  image: medjol, // Replace with actual image URLs
-  description: 'Large, soft, and rich in flavor, Medjool dates are known as the "king of dates".',
-  
-    rating: 4.5,   // Rating out of 5
+import kerman f
 },
 
 //pischio data
