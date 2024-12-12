08f8deb2db587557b673d689dc3ec20cd550542f
FAMEHyder <famehyder9999@gmail.com>
2024-12-12 15:38:28 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -14,14 +14,7 @@ import w180 from '../Image/w180.png'
   Category:'Raisins',
   price:30,
   name: 'Sultana Raisin',
-  image: sul
-  Category:'Walnuts',
-  price:23,
-  name: 'English Walnuts (Persian Walnuts)',
-  image: eng, // Replace with a valid image URL
-  description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
-  
-    rating: 4.5,   // Rating out of 5
+  image: ting out of 5
 },
 ];
 
