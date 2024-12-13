5803908c3564e99aeaa2f4fdb269df0b4e8d3b6c
FAMEHyder <famehyder9999@gmail.com>
2024-12-13 15:39:13 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -19,13 +19,6 @@ import sultana from '../Image/sultana.png';
 {
   id: 21,
   Category:'Cashews',
-  price:120,
-  name: 'W-180 (King of Cashews)',
-  image: w180,
-  description: 'W-180, known as the "King of Cashews," is the largest and highest quality cashew nut available.',
-  
-    rating: 4.5,   // Rating out of 5
-},
 //Dates Data
 {
   id: 33,
