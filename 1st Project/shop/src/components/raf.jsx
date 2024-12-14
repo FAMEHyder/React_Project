ca6fff7a883aa9ba9f0afb6e71748b0c68dc39f0
FAMEHyder <famehyder9999@gmail.com>
2024-12-14 15:43:18 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -2,16 +2,7 @@
 
 
 
-    id: 1,
-    Category:'Almonds',
-    name: 'Nonpareil',
-    image: nonpareil, // Image representing the Nonpareil almond variety
-   
 
-
-  // Appricote Data
-  { 
-    id: 9,
     Category:'Dried Apricots',
     price: 20,
     name: 'Moorpark Apricot',
