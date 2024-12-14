3e2399e733bb7ac881fa3905d6c47775a2360a03
FAMEHyder <famehyder9999@gmail.com>
2024-12-14 15:43:57 +0500

    react

--- 1st Project/shop/src/components/raf.jsx
+++ 1st Project/shop/src/components/raf.jsx
@@ -8,16 +8,7 @@
 
     rating: 4.5,   // Rating out of 5
 },
-
-
-
-},
-//walnutdata
-{
-  id: 65,
-  Category:'Walnuts',
-  price:23,
-  name: 'English Walnuts (Persian Walnuts)',
+ts (Persian Walnuts)',
   image: eng, // Replace with a valid image URL
   description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells.',
   
