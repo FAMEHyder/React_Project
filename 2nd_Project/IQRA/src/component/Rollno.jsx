9640adf9defdf213c36e8db302fa133b170cd7ff
FAMEHyder <famehyder9999@gmail.com>
2025-01-12 15:29:11 +0500

    react

--- 2nd_Project/IQRA/src/component/Rollno.jsx
+++ 2nd_Project/IQRA/src/component/Rollno.jsx
@@ -17,7 +17,7 @@ const RollNoForm = () => {
 
     setError("");
     console.log("Submitted Roll No:", rollNo);
-    // You can perform further actions, lik
+    // You can perform further actions, l
   };
 
   return (
