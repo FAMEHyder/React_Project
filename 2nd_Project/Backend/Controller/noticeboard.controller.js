c047fe570eccd53950512b9849dc17169d3b2b59
FAMEHyder <famehyder9999@gmail.com>
2025-01-18 13:48:09 +0500

    react

---
+++ 2nd_Project/Backend/Controller/noticeboard.controller.js
@@ -0,0 +1,37 @@
+import Notice from '../Models/noticeboard.model.js';
+
+export const addNotice = async (req, res) => {
+    const { title, description, date } = req.body;
+  
+    // Validation
+    if (!title || !description || !date) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+  
+    try {
+      const newNotice = new Notice({ title, description, date });
+      const savedNotice = await newNotice.save();
+      res.status(201).json(savedNotice);
+    } catch (error) {
+      res.status(500).json({ message: 'Error adding notice', error });
+    }
+  };
+
+export const getAllNotices = async (req, res) => {
+  try {
+    const notices = await Notice.find().sort({ date: -1 }); // Sort by newest first
+    res.status(200).json(notices);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching notices', error });
+  }
+};
+
+
+export const deleteAllNotices = async (req, res) => {
+  try {
+    await Notice.deleteMany();
+    res.status(200).json({ message: 'All notices have been deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting notices', error });
+  }
+};
---
+++ 2nd_Project/Backend/Routes/noticeboard.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {addNotice,getAllNotices,deleteAllNotices} from '../Controller/noticeboard.controller.js';
+
+const router = express.Router()
+
+router.post ('/AddToBoard' ,addNotice)
+router.get ('/GetFromBoard' ,getAllNotices)
+router.delete ('/DeleteFromBoard' ,deleteAllNotices)
