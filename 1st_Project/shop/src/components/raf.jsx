6512ae96d1786bf59a19c418c18454f3cb0a138c
FAMEHyder <famehyder9999@gmail.com>
2025-02-03 19:08:39 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -61,5 +61,3 @@ const ProductData = [
   name: 'Medjool',
   image: medjol, // Replace with actual image URLs
   description: 'Large, soft, and rich in flavor, Medjool dates are known as the "king of dates".',
-  
-    rating: 4.5,  
