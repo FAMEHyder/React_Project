f3b7e467b0400f31f7cdf3a62e17aa7586134a2c
FAMEHyder <famehyder9999@gmail.com>
2025-02-05 19:25:58 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -21,5 +21,4 @@ const ProductData = [
     id: 1,
     Category:'Almonds',
     name: 'Nonpareil',
-    image: nonpareil, // Image representing the Nonpareil almond variety
-    description: 'Thin-skinned, easy to blanch, and u
+    imag
