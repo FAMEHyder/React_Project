530109b760b7da36e75d745e033d7f39338665ca
FAMEHyder <famehyder9999@gmail.com>
2025-02-06 19:48:34 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -72,4 +72,4 @@ const ProductData = [
   price:40,
   name: 'Kerman Pistachio',
   image: kerman,
-  description: 'Kerman pistachios are known for their large size, creamy texture, and rich flavor, making the
+  description: 'Kerman pistachios are known for their large size, creamy texture, and r
