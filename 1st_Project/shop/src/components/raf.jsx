a1a73e4b2aefc02419a1048df7c4b66e2d21d63c
FAMEHyder <famehyder9999@gmail.com>
2025-02-07 20:00:33 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -39,7 +39,4 @@ const ProductData = [
     description: 'Moorpark apricots are large, juicy, and known for their sweet, rich flavor. ' + 
                  'Due to their size and sweetness, they are a favorite among apricot lovers.',
                  
-    rating: 4.5,   // Rating out of 5
-  },
-
-//Cashe
+    rating: 4.5,   // 
