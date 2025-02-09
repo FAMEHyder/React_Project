31313e5b42851b8e8a4d892953576ad52e01a18d
FAMEHyder <famehyder9999@gmail.com>
2025-02-09 20:30:25 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -96,4 +96,4 @@ const ProductData = [
   price:23,
   name: 'English Walnuts (Persian Walnuts)',
   image: eng, // Replace with a valid image URL
-  description: 'English walnuts are known for their mild flavor and thin, easy-to-crack shells
+  description: 'English walnuts are known for their mild flavor and t
