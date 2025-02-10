539d8cb636724fd28a8690d9aecd8f46db0b0c84
FAMEHyder <famehyder9999@gmail.com>
2025-02-10 20:35:14 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -51,4 +51,4 @@ const ProductData = [
   image: w180,
   description: 'W-180, known as the "King of Cashews," is the largest and highest quality cashew nut available.',
   
-    rating: 4.5,   // Rating ou
+    rating: 4.5,   // 
