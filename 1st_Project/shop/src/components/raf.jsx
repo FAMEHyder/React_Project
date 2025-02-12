f3df2a26c0f1741961947bdc378fd92c03d2f681
FAMEHyder <famehyder9999@gmail.com>
2025-02-12 21:00:29 +0500

    react

--- 1st_Project/shop/src/components/raf.jsx
+++ 1st_Project/shop/src/components/raf.jsx
@@ -22,22 +22,3 @@ const ProductData = [
     Category:'Almonds',
     name: 'Nonpareil',
     image: nonpareil, // Image representing the Nonpareil almond variety
-    description: 'Thin-skinned, easy to blanch, and used in many almond products. ' +
-                 'Overall, it is one of the most sought-after varieties in the market due to its versatility.',
-    price: 10.99,  // Price of the Nonpareil almond
-    rating: 4.5,   // Rating out of 5
-  },
-
-
-  // Appricote Data
-  { 
-    id: 9,
-    Category:'Dried Apricots',
-    price: 20,
-    name: 'Moorpark Apricot',
-    image: chuli1, // Image representing the Moorpark apricot variety
-    description: 'Moorpark apricots are large, juicy, and known for their sweet, rich flavor. ' + 
-                 'Due to their size and sweetness, they are a favorite among apricot lovers.',
-                 
-    rating: 4.5,   // Rating out of 5
-  },
