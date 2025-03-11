5ff187ec3663991925136ee22ba9f0985e601bfb
FAMEHyder <famehyder9999@gmail.com>
2025-03-11 15:37:38 +0500

    react

--- 2nd_Project/IQRA/src/component/Contact.jsx
+++ 2nd_Project/IQRA/src/component/Contact.jsx
@@ -73,7 +73,7 @@ const ContactUs = () => {
             <PhoneIcon fontSize="large" color="primary" />
             <Typography variant="h6">Phone</Typography>
             <Typography variant="body2">+923445254658</Typography>
-            <Typography variant="body2">+923464640837</Typography>
+            <Typography variant="body2">+923450881428</Typography>
           </Box>
         </Grid>
         <Grid item xs={12} sm={6} md={3}>
