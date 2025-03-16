0104c26fdbfdc42a62d1be1fd32a9c86b897a60b
FAMEHyder <famehyder9999@gmail.com>
2025-03-16 21:30:00 +0500

    react

---
+++ DMP/Backend/Database/Database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+
+const DataBaseConnection = async ()=>{
+    try {
+        await mongoose.connect(process.env.MANGO_URL)
+        console.log("Connected to Database successfully")
+    }
+    catch{
+        console.log("Database Connection Filed")
+    }
+
+
+}
+
+mongoose.connection.on("Disconnected",()=>{
+    console.log("Disconnected")
+})
+
+export default DataBaseConnection;
