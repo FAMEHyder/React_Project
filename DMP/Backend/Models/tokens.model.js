7eae5634de9ad30101661e77b7c9c7c98855836a
FAMEHyder <famehyder9999@gmail.com>
2025-03-22 21:16:42 +0500

    react

--- DMP/Backend/Models/tokens.model.js
+++ DMP/Backend/Models/tokens.model.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
 const tokenschema = new mongoose.schema ({
-    
+    AccessToken:{
+        type : Number,
+        required : true,
+        trim : true
+
+    }
 })
